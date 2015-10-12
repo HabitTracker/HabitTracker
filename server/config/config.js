ef8ab3f24dc0101b6a71ba2af3749821479a5948
myazhbin <myazhbin@gmail.com>
2015-10-13 06:27:45 +0800

    modified styles.css to make ionic display properly

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,7 @@ try {
 catch (err) {}
 
 module.exports = {
-  port: 8080,
+  port: 8100,
   localdb: 'mongodb://127.0.0.1:27017/habitdb',
   tokenSecret: process.env.tokenSecret || secret.tokenSecret,
   googleSecret: process.env.googleSecret || secret.googleSecret
