22c3ebfc2ea239e1b3c0b65901bb296301fa8672
Mike Hsieh <mythsieh@gmail.com>
2015-11-17 15:18:37 -0800

    Edited calendar

--- client/app/calendar/calendar.js
+++ client/app/calendar/calendar.js
@@ -15,12 +15,12 @@ angular.module('app.calendar', ['ui.calendar', 'ui.bootstrap'])
     };
     /* event source that contains custom events on the scope */
     $scope.events = [
-      {title: 'Pushups',start: new Date(y, m, 1)},
-      {title: 'Situps',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
+      {title: 'Code',start: new Date(y, m, 1), end: new Date(y, m, 13)},
+      {title: 'Eat',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
       // {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
       // {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
       {title: 'Sleep',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
-      {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
+      // {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
     ];
     /* event source that calls a function on every view switch */
     $scope.eventsF = function (start, end, timezone, callback) {
